777a4f55738c8109d6beeb5ebd67d761a0913b4d
ARCANIT78 <loisgordisrour@gmail.com>
2025-04-02 16:07:49 -0400

    Añadir métodos web y actualizar diseño de base de datos
    Se han añadido nuevos métodos web en `WebService1.asmx.vb`, incluyendo `ObtenerLugares` y `RegistrarNuevoAfiliado`, para gestionar datos de lugares y registrar afiliados. Se ha mantenido la función `HelloWorld`.
    
    En `db.xss`, se han ajustado las posiciones de los elementos y las relaciones entre tablas, lo que puede afectar la representación visual del modelo de datos.

--- WS-Seguros/WebService1.asmx.vb
+++ WS-Seguros/WebService1.asmx.vb
@@ -4,15 +4,32 @@ Imports System.Web.Services.Protocols
 
 ' Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente.
 ' <System.Web.Script.Services.ScriptService()> _
-<System.Web.Services.WebService(Namespace:="http://tempuri.org/")> _
-<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
-<ToolboxItem(False)> _
+<System.Web.Services.WebService(Namespace:="http://tempuri.org/")>
+<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)>
+<ToolboxItem(False)>
 Public Class WebService1
     Inherits System.Web.Services.WebService
 
-    <WebMethod()> _
+    <WebMethod()>
     Public Function HelloWorld() As String
         Return "Hola a todos"
+    End Function
+    <WebMethod()>
+    Public Function ObtenerLugares() As db.TLugaresDataTable
+        Dim adap As New dbTableAdapters.TLugaresTableAdapter
+        Dim ds As New db.TLugaresDataTable
+        adap.Fill(ds)
+        Return ds
+    End Function
+    <WebMethod>
+    Public Function RegistrarNuevoAfiliado(CI As String, nombre As String, paterno As String, materno As String, FechaNacimiento As Date, sexo As String) As String
+        Try
+            Dim adap As New dbTableAdapters.TPersonasTableAdapter
+            adap.Insert(CI, nombre, paterno, materno, FechaNacimiento, sexo, "ALTA")
+            Return "Afiliado registrado correctamente"
+        Catch ex As Exception
+            Return "Error: no se pudo registrar al nuevo afiliado"
+        End Try
     End Function
 
 End Class
